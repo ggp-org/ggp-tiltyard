15be30c7f75fed4902d8bfd1a6aa247f82ecb4b4
Sam Schreiber <sam.schreiber@gmail.com>
2011-01-23 06:55:26 +0000

    Added a cron job that runs a mock scheduler, to verify that the cron mechanism is working.

---
+++ src/ggp/apollo/DataPoint.java
@@ -0,0 +1,73 @@
+package ggp.apollo;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.jdo.JDOObjectNotFoundException;
+import javax.jdo.PersistenceManager;
+import javax.jdo.annotations.*;
+
+import org.datanucleus.store.query.AbstractQueryResult;
+
+@PersistenceCapable
+public class DataPoint {
+    @PrimaryKey @Persistent private String theDataPoint;
+
+    public DataPoint(String theData) {
+        this.theDataPoint = theData;        
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        pm.makePersistent(this);
+        pm.close();
+    }
+    
+    public String getData() {
+        return theDataPoint;
+    }
+
+    /* Static accessor methods */
+    public static Set<DataPoint> loadData() throws IOException {
+        Set<DataPoint> theData = new HashSet<DataPoint>();
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        try {
+            Iterator<?> sqr = ((AbstractQueryResult) pm.newQuery(DataPoint.class).execute()).iterator();
+            while (sqr.hasNext()) {
+                theData.add((DataPoint)sqr.next());
+            }            
+        } catch(JDOObjectNotFoundException e) {
+            ;
+        } finally {
+            pm.close();
+        }
+        return theData;
+    }
+    
+    public static DataPoint loadDataPoints(String theKey) throws IOException {
+        DataPoint theData = null;
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        try {
+            theData = pm.detachCopy(pm.getObjectById(DataPoint.class, theKey));
+        } catch(JDOObjectNotFoundException e) {
+            ;
+        } finally {
+            pm.close();
+        }
+        return theData;
+    }
+    
+    public static void clearMatches() throws IOException {
+        Set<DataPoint> theData = loadData();
+
+        for (DataPoint m : theData) {
+            PersistenceManager pm = PMF.get().getPersistenceManager();
+            try {
+                pm.deletePersistent(pm.getObjectById(DataPoint.class, m.getData()));
+            } catch(JDOObjectNotFoundException e) {
+                ;
+            } finally {
+                pm.close();
+            }
+        }
+    }
+}
--- src/ggp/apollo/GGP_ApolloServlet.java
+++ src/ggp/apollo/GGP_ApolloServlet.java
@@ -1,13 +1,30 @@
 package ggp.apollo;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class GGP_ApolloServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Hello, world! This is Apollo.");
+        if (req.getRequestURI() == "/cron/scheduling_round") {
+            runSchedulingRound();
+            resp.setContentType("text/plain");
+            resp.getWriter().println("Starting scheduling round.");            
+            return;
+        }
+        
+        resp.getWriter().println("<html><body>");
+        resp.getWriter().println("Hello, world! This is Apollo.<ul>");        
+        for(DataPoint d : DataPoint.loadData()) {
+            resp.getWriter().println("<li>" + d.getData());
+        }
+        resp.getWriter().println("</ul></body></html>");
+    }
+    
+    public void runSchedulingRound() {
+        new DataPoint("Scheduling round: " + new Date().toString());
     }
 }
