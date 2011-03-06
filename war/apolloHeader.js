aef74724908abb4e643afb4abb46359a6ea9238d
Sam Schreiber <sam.schreiber@gmail.com>
2011-03-06 07:31:53 +0000

    Restyle the top-nav, improve the splash page, and improve the way we show information about players.

--- war/apolloHeader.js
+++ war/apolloHeader.js
@@ -2,13 +2,13 @@ var loginNascarHTML;
 function generateHeader(theDiv) {
     var theHTML = "";
     theHTML += '<center>';
-    theHTML += '<table>';
+    theHTML += '<table style="width: 100%; border: 0; margin: 0; border-spacing: 0px 0px;">';
     theHTML += '  <tr>';
     theHTML += '    <td width=30% align="right" valign="bottom"><a class=biglink>Games</a></td>';
     theHTML += '    <td width=15% align="center" valign="bottom"><a class=biglink href="/players/">Players</a></td>';
-    theHTML += '    <td width=10% align="center"><a href="/"><img src="/static/images/ApolloGamingLogo.png" width=200 height=200></img></a></td>';
+    theHTML += '    <td width=10% align="center" style="padding: 0;"><a href="/"><img width=200 src="/static/images/SolidSunSection2.png"></img></a></td>';
     theHTML += '    <td width=15% align="center" valign="bottom"><a class=biglink href="/matches/">Matches</a></td>';
-    theHTML += '    <td width=30% align="left" valign="bottom"><a class=biglink>Stats</a></td>';
+    theHTML += '    <td width=30% align="left" valign="bottom"><a class=biglink>Leaderboard</a></td>';
     theHTML += '  </tr>';
     theHTML += '  <tr>';
     theHTML += '    <td colspan=5 align="center" class="login">';
@@ -37,4 +37,35 @@ function generateHeader(theDiv) {
       }
     }      
     document.getElementById('login_div').innerHTML = loginHTML;    
+}
+
+function generatePlayerHTML(aPlayer) {
+    var thePlayerHTML = '<table class="player" style="background-color:';
+    if ("theURL" in aPlayer) {
+        thePlayerHTML += '#CCEECC';
+    } else {
+        thePlayerHTML += '#DDDDDD';
+    }
+    thePlayerHTML += '"><tr>';
+    thePlayerHTML += '<td width=5></td>';
+    thePlayerHTML += '<td><table style="border-width: 2px; border-style: inset;" cellspacing=0 cellpadding=0><tr><td><img src="http://placekitten.com/g/50/50"/></tr></td></table></td>';
+    thePlayerHTML += '<td width=5></td>';
+    thePlayerHTML += '<td><a style="text-decoration:none; color: #222222;" href="/players/' + aPlayer.name + '"><font size=6><b>' + aPlayer.name + '</b></font><br>';
+    if (aPlayer.visibleEmail.length > 0) {
+        thePlayerHTML += '<tt>' + aPlayer.visibleEmail + '</tt>';
+    } else {
+        thePlayerHTML += '<i>Email address not listed.</i>';
+    }
+    thePlayerHTML += '</a></td>';
+    thePlayerHTML += '<td width=5></td>';
+    thePlayerHTML += '<td>';
+    if (aPlayer.isEnabled) {
+        thePlayerHTML += '<table class="enabled"><tr><td>Enabled!</td></tr></table>';
+    } else {
+        thePlayerHTML += '<table class="disabled"><tr><td>Disabled</td></tr></table>'; 
+    }
+    thePlayerHTML += '<br>';
+    thePlayerHTML += '<table class="gdlversion"><tr><td>' + aPlayer.gdlVersion + '</td></tr></table>';
+    thePlayerHTML += '</td></tr></table>';
+    return thePlayerHTML;
 }
