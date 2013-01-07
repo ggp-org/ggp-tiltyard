358245fd57525879311946e6d32b36ca8480685e
Sam Schreiber <sam.schreiber@gmail.com>
2013-01-07 09:54:20 +0000

    Drop the "snakeAssemblit" game from the Tiltyard whitelist.

--- src/ggp/tiltyard/scheduling/Scheduling.java
+++ src/ggp/tiltyard/scheduling/Scheduling.java
@@ -51,7 +51,7 @@ public class Scheduling {
 		"eightPuzzle", "escortLatch", "ghostMaze2p", "god", "golden_rectangle", "knightThrough", "knightsTour",
 		"lightsOut", "max_knights", "maze", "nineBoardTicTacToe", "numberTicTacToe", "pacman2p", "pacman3p",
 		"pawnToQueen", "pawnWhopping", "peg", "pegEuro", "pentago", "pentagoSuicide",
-		"qyshinsu", "sheepAndWolf", "snake2p", "snakeAssemblit", "snakeParallel",
+		"qyshinsu", "sheepAndWolf", "snake2p", "snakeParallel",
 		"ttcc4_2player", "tictactoe_3player", "ticTacToe", "ticTacToeLarge", "ticTacToeLargeSuicide",
 		"ticTacToeParallel", "ticTacToeSerial", "ticTicToe",
 	};
